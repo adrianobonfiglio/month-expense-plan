b91991eaf53f43f224630778023c22c45d5f828d
Adriano Bonfiglio <adriano.bonfiglio@sap.com>
2019-11-27 00:33:53 -0300

    Change layout to Expanded widget

--- lib/bill/card.dart
+++ lib/bill/card.dart
@@ -20,95 +20,79 @@ class BillCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.only(left: 0, right: 0, bottom: 8, top: 8),
-      child: Container(
+      child: SizedBox(
         height: 90,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 90,
-                  width: getWidth(context, 0.020),
-                  color: bill.category.color,
-                ),
-              ],
-            ),
             Container(
-              width: getWidth(context, 0.5),
-              height: 90,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    alignment: Alignment.topLeft,
-                    padding: EdgeInsets.only(top: 10, left: 5),
-                    child: Text(
+              width: 10,
+              child: Container(
+                color: bill.category.color,
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
                       bill.name,
+                      overflow: TextOverflow.ellipsis,
                       style: TextStyle(
                           fontFamily: FONT_FAMILY,
                           fontWeight: FontWeight.bold,
-                          fontSize: 18),
+                          fontSize: 18,
+                      ),
                     ),
-                  ),
-                  Container(
-                    height: 40,
-                    alignment: Alignment.bottomLeft,
-                    padding: EdgeInsets.only(bottom: 10, left: 5),
-                    child: Text(
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Text(
                       "Venc. dia " + bill.dueDate,
                       style: TextStyle(
                           fontFamily: FONT_FAMILY,
                           fontWeight: FontWeight.bold,
                           fontSize: 14),
                     ),
-                  )
-                ],
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                  ],
+                ),
               ),
             ),
-            Container(
-              width: getWidth(context, 0.4),
-              height: 90,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    alignment: Alignment.topRight,
-                    padding: EdgeInsets.only(top: 10, right: 10),
-                    child: Text(
+            Expanded(
+              flex: 4,
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
                       "R\$ ${bill.amount}",
                       style: TextStyle(
                           fontFamily: FONT_FAMILY,
                           fontWeight: FontWeight.bold,
                           fontSize: 18),
                     ),
-                  ),
-                  Container(
-                      height: 40,
-                      alignment: Alignment.bottomRight,
-                      padding: EdgeInsets.only(bottom: 10, right: 10),
-                      child: Container(
-                        padding: EdgeInsets.only(
-                            left: 10, right: 10, top: 4, bottom: 4),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            color: bill.category.color),
-                        child: Text(bill.category.name,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                                color: Colors.white)),
-                      ))
-                ],
+                    Padding(padding: EdgeInsets.only(top: 20)),
+                    Container(
+                      padding: EdgeInsets.only(
+                          left: 10, right: 10, top: 4, bottom: 4),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: bill.category.color),
+                      child: Text(bill.category.name,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Colors.white)),
+                    )
+                  ],
+                ),
               ),
-            ),
+            )
           ],
-        ),
-      ),
+        )
+      )
     );
   }
 }
